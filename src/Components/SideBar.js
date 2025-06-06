12c31dcab1083093670ca22b1b11110c4a7eb7f7
NishadiGunasinghe <nishadig816@gmail.com>
2025-06-06 22:23:38 +0100

    Created side navigation bar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,51 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../asserts/styles/SideBar.css";
 
-
-// const styles = {
-//   sidemenu: {
-//     backgroundColor: "#092561",
-//     position: "fixed",
-//     top: 0,
-//     left: 0,
-//     height: "100vh",
-//     width: "250px",
-//     padding: "1rem",
-//     boxShadow: "2px 0 5px rgba(0,0,0,0.1)",
-//     zIndex: 1000,
-//   },
-//   closeBtn: {
-//     position: "absolute",
-//     top: "10px",
-//     right: "10px",
-//     fontSize: "1.5rem",
-//     cursor: "pointer",
-//     background: "none",
-//     border: "none",
-//   },
-//   profileCard: {
-//     marginTop: "2.5rem",
-//     marginBottom: "1rem",
-//     padding: "1rem",    
-//   },
-//   cardTitle: {
-//     color: "#1569b3",
-//     fontSize: "2.25rem",
-//     marginTop: "0.5rem",
-//     fontFamily: "cursive"
-//   },
-//   navLink: {
-//     display: "block",
-//     marginTop: "1rem",
-//     color: "#1569b3",
-//     textDecoration: "none",
-//     fontSize: "1.3rem",
-//     paddingLeft: "1rem",
-//     fontFamily: "cursive"
-//   },
-  
-// }
-
 export default ({ toggleMenu }) => (
   <div className="sidemenu">
     <button className="closeBtn" onClick={toggleMenu}>
